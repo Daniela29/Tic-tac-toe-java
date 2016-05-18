a38e66fb225c2a42039c3e7c39241d8af4d76ab5
Daniela29 <daniela.albastroiu@gmail.com>
2016-05-18 18:19:50 +0300

    Added Java project

---
+++ Tictactoe.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Tictactoe extends JFrame {
+
+    JPanel p= new JPanel();
+    
+    public static void main(String[] args) {
+       
+    JFrame Tictactoe= new TictactoeFrame();
+    Tictactoe.setTitle("TicTacToe Game!");
+    Tictactoe.setSize(600,600);
+    Tictactoe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Tictactoe.setLocationRelativeTo(null);
+    Tictactoe.setVisible(true);
+    
+    }
+    
+}
